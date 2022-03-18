59de73d15ab6a0631e1ca8a07ca8ccbd5f97dbe4
Jakub Povinec <jakub.povinec@gmail.com>
2022-03-18 12:48:12 +0100

    added jwt support

--- src/routes/users/auth.js
+++ src/routes/users/auth.js
@@ -2,15 +2,18 @@ const express = require('express');
 const router = express.Router();
 const User = require('../../models/Users');
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken');
 
 // Register enpoint
 router.post('/register', async (req, res) => {
+    // Checks whether username or email already exists
     const emailExists = await User.exists( { 'credentials.email': req.body.email });
     if (emailExists) return res.status(409).send({ error: { message: 'Email already in use' }});
     
     const usernameExists = await User.exists({ 'credentials.username': req.body.username });
     if (usernameExists) return res.status(409).send({ error: { message: 'Username already in use' }});
 
+    // Creates a hash from the specified password
     const passwdHash = await bcrypt.hash(req.body.password, 10);
 
     const newUser = await User.create({
@@ -24,7 +27,15 @@ router.post('/register', async (req, res) => {
             password: passwdHash
         }
     });
-    res.status(201).send(newUser);
+
+    // Generate token
+    const token = jwt.sign({ _id: newUser._id }, process.env.JWT_SECRET);
+
+    res.status(201).send({
+        _id: newUser._id,
+        username: newUser.credentials.username,
+        token: token
+    });
 });
 
 
@@ -41,7 +52,14 @@ router.post('/login', async (req, res) => {
     const correctPassword = await bcrypt.compare(req.body.password, user.credentials.password)
     if (!correctPassword) return res.status(404).send({ error: { message: 'Password or username is wrong' }});
 
-    res.status(200).send({ message: 'Successful login' })
+    // Generate token
+    const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET);
+
+    res.status(200).send({
+        _id: user._id,
+        username: user.credentials.username,
+        token: token
+    });
 });
 
 module.exports = router;
