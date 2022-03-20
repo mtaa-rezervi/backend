4268bcb52e6f7a0a82236ebc4a94c8cdf4c2f0ce
Jakub Povinec <jakub.povinec@gmail.com>
2022-03-20 14:52:06 +0100

    added model Reservations and redesigned Rooms and Users

--- src/models/Rooms.js
+++ src/models/Rooms.js
@@ -1,19 +1,31 @@
 const mongoose = require('mongoose');
+const Schema = mongoose;
 
+/*
 const reservationsSubSchema = mongoose.Schema({
     name: String
 }, { _id : false });
+*/
 
 const roomSchema = new mongoose.Schema({
-    name: String,
-    floor: Number,
-    room_number: Number,
-    info: String,
-    number_of_seats: Number,
+    name: { type: String, default: '' },
+    floor: { type: Number, default: -100 },
+    room_number: { type: Number, default: -100 },
+    info: { type: String, default: '' },
+    address: {
+        street: { type: String, default: '' },
+        city: { type: String, default: '' },
+        state: { type: String, default: '' },
+        zip: { type: String, default: '' },
+    }, 
+    owner_id: { type: Schema.Types.ObjectId, ref: 'User' },
+    number_of_seats: { type: Number, default: -100 },
     amenities: [String],
-    thumbnail_url: String,
+    thumbnail_url: { type: String, default: '' },
     image_urls: [String],
-    reservations: [reservationsSubSchema]
+    reservations: [{ type: Schema.Types.ObjectId, ref: 'Reservation' }],
+    created_at: { type: Date, default: Date.now },
+    updated_at: { type: Date, default: Date.now }
 }, {versionKey: false});
 
 module.exports = mongoose.model('Room', roomSchema);
--- src/models/Users.js
+++ src/models/Users.js
@@ -1,17 +1,5 @@
 const mongoose = require('mongoose');
-
-/*
-const name = mongoose.Schema({
-    first_name: String,
-    last_name: String
-}, { _id : false });
-
-const credentials = mongoose.Schema({
-    email: String,
-    username: String,
-    password: String
-}, { _id : false });
-*/
+const Schema = mongoose;
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -22,7 +10,14 @@ const userSchema = new mongoose.Schema({
         email: { type: String, required: true },
         username: { type: String, required: true },
         password: { type: String, required: true },
-    }
+    }, 
+    profile_pic: { type: String, default: '' },
+    reservation_history: [{ type: Schema.Types.ObjectId, ref: 'Reservation' }],
+    active_reservations: [{ type: Schema.Types.ObjectId, ref: 'Reservation' }],
+    active_listings: [{ type: Schema.Types.ObjectId, ref: 'Room' }],
+    notifications: [{ type: Object }],
+    created_at: { type: Date, default: Date.now },
+    updated_at: { type: Date, default: Date.now }
 }, {versionKey: false});
 
 module.exports = mongoose.model('User', userSchema);
