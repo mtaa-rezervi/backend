093480eb449862811975e6fe9844eaac71d2d9b6
filippetran <filippetran@gmail.com>
2022-03-26 13:57:06 +0100

    added filtering by name, gte, lte

--- src/routes/rooms/getRooms.js
+++ src/routes/rooms/getRooms.js
@@ -5,8 +5,21 @@ const Room = require('../../models/Rooms');
 
 // Returns a list of rooms
 router.get('/', middleware.verifyJWT, async (req, res) => {
-    const room = await Room.find();
-    res.send(room);
+    let mongoQuery = {};
+    
+    // name
+    req.query.name ? (mongoQuery.name = req.query.name) : '';
+    // gte
+    req.query.num_of_seats_gte ? (mongoQuery.number_of_seats = { $gte: req.query.num_of_seats_gte }) : '';
+    // lte
+    req.query.num_of_seats_lte ? (mongoQuery.number_of_seats = { $lte: req.query.num_of_seats_lte }) : '';
+    // gte && lte
+    (req.query.num_of_seats_gte && req.query.num_of_seats_lte) ? (mongoQuery.number_of_seats = { $gte: req.query.num_of_seats_gte, $lte: req.query.num_of_seats_lte }) : '';
+
+    // Find Rooms based on specified query
+    const rooms = await Room.find(mongoQuery);
+    console.log(mongoQuery, rooms.length);
+    res.send(rooms);
 });
 
 module.exports = router;
