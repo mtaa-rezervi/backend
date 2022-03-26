e70cf70bd56be1d9e93c6674a759000c3b6b0299
filippetran <filippetran@gmail.com>
2022-03-26 14:24:02 +0100

    added filtering by amenities, owner_id

--- src/routes/rooms/getRooms.js
+++ src/routes/rooms/getRooms.js
@@ -15,6 +15,15 @@ router.get('/', middleware.verifyJWT, async (req, res) => {
     req.query.num_of_seats_lte ? (mongoQuery.number_of_seats = { $lte: req.query.num_of_seats_lte }) : '';
     // gte && lte
     (req.query.num_of_seats_gte && req.query.num_of_seats_lte) ? (mongoQuery.number_of_seats = { $gte: req.query.num_of_seats_gte, $lte: req.query.num_of_seats_lte }) : '';
+    // amenity
+    if(req.query.amenity){
+        mongoQuery.amenities = { $all: req.query.amenity }
+    }
+    // owner id
+    if(req.query.owner_id){
+        mongoQuery.owner_id = req.query.owner_id;
+    }
+
 
     // Find Rooms based on specified query
     const rooms = await Room.find(mongoQuery);
