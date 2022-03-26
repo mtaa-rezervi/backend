22f6e34dc105b9d43627da2a9d7b2ef1a69a4beb
filippetran <filippetran@gmail.com>
2022-03-26 14:43:33 +0100

    refactored code for filtering

--- src/routes/rooms/getRooms.js
+++ src/routes/rooms/getRooms.js
@@ -8,13 +8,21 @@ router.get('/', middleware.verifyJWT, async (req, res) => {
     let mongoQuery = {};
     
     // name
-    req.query.name ? (mongoQuery.name = req.query.name) : '';
-    // gte
-    req.query.num_of_seats_gte ? (mongoQuery.number_of_seats = { $gte: req.query.num_of_seats_gte }) : '';
-    // lte
-    req.query.num_of_seats_lte ? (mongoQuery.number_of_seats = { $lte: req.query.num_of_seats_lte }) : '';
-    // gte && lte
-    (req.query.num_of_seats_gte && req.query.num_of_seats_lte) ? (mongoQuery.number_of_seats = { $gte: req.query.num_of_seats_gte, $lte: req.query.num_of_seats_lte }) : '';
+    if(req.query.name){
+        mongoQuery.name = req.query.name;
+    }
+
+    // num_of_seats_gte, num_of_seats_lte
+    if(req.query.num_of_seats_gte && req.query.num_of_seats_lte){
+        mongoQuery.number_of_seats = { $gte: req.query.num_of_seats_gte, $lte: req.query.num_of_seats_lte };
+    }
+    else if(req.query.num_of_seats_gte){
+        mongoQuery.number_of_seats = { $gte: req.query.num_of_seats_gte };
+    }
+    else if(req.query.num_of_seats_lte){
+        mongoQuery.number_of_seats = { $lte: req.query.num_of_seats_lte }
+    }
+
     // amenity
     if(req.query.amenity){
         mongoQuery.amenities = { $all: req.query.amenity }
