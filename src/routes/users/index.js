c42d37d5aba50f9963e75da81581494fb43a86c8
filippetran <filippetran@gmail.com>
2022-03-28 23:46:51 +0200

    added routes for new endpoints

--- src/routes/users/index.js
+++ src/routes/users/index.js
@@ -4,5 +4,7 @@ const router = express.Router();
 // /users routes //
 router.use('/login', require('./login'));
 router.use('/register', require('./register'));
+router.use('/:id', require('./getUser'));
+router.use('/:id/history', require('./history'));
 
 module.exports = router;
