633c64767ed6318fe9653392c56736adc967ba4b
Jakub Povinec <jakub.povinec@gmail.com>
2022-04-12 21:28:46 +0200

    added noti sorting

--- src/routes/users/notifications.js
+++ src/routes/users/notifications.js
@@ -8,8 +8,9 @@ const router = express.Router({ mergeParams: true });
 // Returns a list of users notifications
 router.get('/', verifyJWT, verifyUserID, async (req, res) => {
     try {
-        const user = await Users.findById(mongoose.Types.ObjectId(req.params.id), '_id notifications').sort({time: 'desc'});
+        const user = await Users.findById(mongoose.Types.ObjectId(req.params.id), '_id notifications');
         if (!user) throw 'Record doesnt exist';
+        user.notifications = user.notifications.sort((a, b) => new Date(b.time) - new Date(a.time));
         res.send(user)
     } catch (err) {
         res.status(404).send({ error: { message: 'Record doesnt exist' } });
