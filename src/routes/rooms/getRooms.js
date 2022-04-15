8ab3fa89e2c6a88a056ba89b8665bd20f711bdc1
Jakub Povinec <jakub.povinec@gmail.com>
2022-04-15 21:53:53 +0200

    added support for address queries (for real now)

--- src/routes/rooms/getRooms.js
+++ src/routes/rooms/getRooms.js
@@ -9,10 +9,16 @@ const router = express.Router();
 router.get('/', verifyJWT, async (req, res) => {
     let mongoQuery = {};
     
-    req.query.name ? (mongoQuery.name = { $regex: req.query.name }) : '';
+    req.query.name ? (mongoQuery.name = new RegExp(`^${req.query.name}$`)) : '';
     req.query.amenity ? (mongoQuery.amenities = { $all: req.query.amenity }) : '';
     req.query.owner_id ? (mongoQuery.owner_id = req.query.owner_id) : '';
     req.query.id ? (mongoQuery._id = { $in: req.query.id }) : '';
+    req.query.street ? (mongoQuery['address.street'] = new RegExp(`^${req.query.street}$`)) : '';
+    req.query.city ? (mongoQuery['address.city'] = new RegExp(`^${req.query.city}$`)) : '';
+    req.query.zip ? (mongoQuery['address.zip'] = new RegExp(`^${req.query.zip}$`)) : '';
+    req.query.state ? (mongoQuery['address.state'] = new RegExp(`^${req.query.state}$`)) : '';
+
+    console.log(mongoQuery)
 
     // num_of_seats_gte, num_of_seats_lte
     if (req.query.num_of_seats_gte && req.query.num_of_seats_lte) {
